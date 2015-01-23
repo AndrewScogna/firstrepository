9cb936a6ad1be4208adc65220e440648a7e5a180
Andrew Scogna <Andrew Scogna>
2015-01-23 16:42:03 -0500

    Mike

---
+++ assignment8/index.js
@@ -0,0 +1,28 @@
+/* js asssignment 8 */
+
+var text = ["Kaias", "Pedro", "Hotfoot", "Penelope"];
+console.log(myNames.length)
+console.log(myNames.substring(0, 2))
+    // Locate "P" inside array text
+
+for (var i = 0; i < text.length; i++) {
+    if (text[i])(i === "P")
+}
+
+
+for (var i = 0; 1 < 10; i ++) {
+    console.log(i);
+}
+
+var counter = 0
+for (counter = 0) counter < 11;
+counter++; {
+    while (I < 11 _)
+
+}
+console.log[counter];
+
+var car = {
+ doors: "4"
+};
+console.log("Honk!);
