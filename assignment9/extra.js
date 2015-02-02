e4c5eb4bdcc473f9f6d03e1f7573257a502f4c01
Andrew Scogna <Andrew Scogna>
2015-02-02 10:24:25 -0500

    resume
    I’ve made a lot of changes, however I’m not making a lot of progress.

---
+++ assignment9/extra.js
@@ -0,0 +1,19 @@
+ <script>
+    var name = "Andrew";
+    console.log(name);
+
+    document.getElementById("box2").style.background = "maroon";
+
+    var response = prompt("What is your first name?");
+    myFunction(response);
+
+
+    function myFunction() {
+        alert("Hello " + response);
+    }
+    </script>
+
+
+    $("li").hasclass("span");
+fadeIn(); (400ms).hover();
+fadeOut(); (600ms).hover();
